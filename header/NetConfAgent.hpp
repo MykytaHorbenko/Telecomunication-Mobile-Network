bdb57408895cf9d866d5850d5be06a4917d66c29
MykytaHorbenko <84394444+MykytaHorbenko@users.noreply.github.com>
2021-06-11 12:39:44 +0300

    Add files via upload

--- header/NetConfAgent.hpp
+++ header/NetConfAgent.hpp
@@ -3,27 +3,30 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 
 #include "libsysrepocpp/headers/Session.hpp"
 //#include <libsysrepocpp/headers/Session.hpp>
 using namespace sysrepo;
 
+namespace Netconfagent
+{
 
 class NetConfAgent
 {
+    public:
+    bool initSysrepo();
+    bool fetchData(string xpath);
+    bool subscribeForModelChanges(string _module_name);
+    bool registerOperData(string _module_name);
+    bool subscribeForRpc(string _module_name, string _rpc_xpath);
+    bool notifySysrepo(string _module_name);
+    bool changeData(string _xpath, string newValue);
     private:
     S_Connection Connection;
     S_Session Session;
     S_Subscribe Subscribe;
-    public:
-    bool initSysrepo();
-    bool closeSysrepo();
-    bool fetchData();
-    bool subscribeForModelChanges();
-    bool registerOperData();
-    bool subscribeForRpc();
-    bool notifySysrepo();
-    bool changeData();
     
 };
 
+}
