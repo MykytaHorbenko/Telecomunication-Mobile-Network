f4b44ffb36e0b2cc8db228d19560ae297983f142
MykytaHorbenko <84394444+MykytaHorbenko@users.noreply.github.com>
2021-06-12 20:51:50 +0300

    Add files via upload

--- header/NetConfAgent.hpp
+++ header/NetConfAgent.hpp
@@ -6,7 +6,7 @@
 #include <string>
 
 #include "libsysrepocpp/headers/Session.hpp"
-//#include <libsysrepocpp/headers/Session.hpp>
+
 using namespace sysrepo;
 
 namespace Netconfagent
@@ -15,13 +15,70 @@ namespace Netconfagent
 class NetConfAgent
 {
     public:
+/*
+*@brief initSysrepo. 
+*This method initializes a session with the server
+*
+*@return true if ok, otherwise false
+*/
     bool initSysrepo();
-    bool fetchData(string xpath);
-    bool subscribeForModelChanges(string _module_name);
-    bool registerOperData(string _module_name);
-    bool subscribeForRpc(string _module_name, string _rpc_xpath);
-    bool notifySysrepo(string _module_name);
-    bool changeData(string _xpath, string newValue);
+/*
+*@brief brief fetchData. 
+*This method receives data from the model and writes to the container
+*
+*@param xpath the path on which the data will be received
+*@param map the container to which the data will be written
+*
+*@return true if ok, otherwise false
+*/
+    bool fetchData(string xpath, map <std::string, std::string>& map);
+/*
+*@brief subscribeForModelChanges. 
+*This method reacts to changes in the model and prints them
+*
+*@param module_name the name of module to which the subscription is created
+*
+*@return true if ok, otherwise false
+*/
+    bool subscribeForModelChanges(std::string _module_name);
+/*
+*@brief registerOperData. 
+*This method receives operdata from the model and set them
+*
+*@param module_name the name of module to which the subscription is created
+*
+*@return true if ok, otherwise false
+*/
+    bool registerOperData(std::string _module_name);
+/*
+*@brief subscribeForRpc. 
+*This method makes RPC call and receives return value from the model
+*
+*@param module_name the name of module to which the subscription is created
+*@param rpc_xpath the path on which the return value will be received
+*
+*@return true if ok, otherwise false
+*/
+    bool subscribeForRpc(std::string _module_name, std::string _rpc_xpath);
+/*
+*@brief notifySysrepo. 
+*This method sends notification in model
+*
+*@param module_name the name of module to which the notification is sent
+*
+*@return true if ok, otherwise false
+*/
+    bool notifySysrepo(std::string _module_name);
+ /*
+*@brief changeData. 
+*This method change value of leaf in model
+*
+*@param xpath the path on which the data will be changed
+*@param newValue new value of leaf
+*
+*@return true if ok, otherwise false
+*/   
+    bool changeData(std::string _xpath, std::string newValue);
     private:
     S_Connection Connection;
     S_Session Session;
